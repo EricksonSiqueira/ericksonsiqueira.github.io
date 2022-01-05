496f2074fb98e41a30e4329b0a5500ddfa63ee90
Erickson <ericksonsiqueirasilva@gmail.com>
2022-01-04 23:13:31 -0300

    Adicionando footer e header na pagina de portifolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer'
 
 function Portfolio() {
   return (
     <div>
+      <Header />
       Página de portfolio
+      <Footer />
     </div>
   );
 }
