88ff6aead1202bfa10ba31a9061e5a2f34650a7f
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-02 13:16:46 -0300

    feat: cria header

---
+++ src/components/header/HeaderStyle.js
@@ -0,0 +1,16 @@
+import style from "styled-components";
+
+export const HeaderStyled = style.header`
+  background: ${props => props.theme.colors.darkPurple};
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+`;
+
+export const ButtonIcon = style.button`
+  background: ${props => props.theme.colors.darkPurple};
+  margin: 0px 10px;
+  border: none;
+`;
