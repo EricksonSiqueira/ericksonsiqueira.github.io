f59b8893aab90620a02e5369da7bc897f97b9441
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-02 16:59:53 -0300

    fix: medias para min no lugar de max

--- src/themes/default.js
+++ src/themes/default.js
@@ -16,10 +16,10 @@ export default {
   },
   // Breakpoints for responsive design
   breakpoints: {
-    xsm: 'screen and (max-width: 320px)',
-    sm: 'screen and (max-width: 640px)',
-    md: 'screen and (max-width: 768px)',
-    lg: 'screen and (max-width: 1024px)',
-    xl: 'screen and (max-width: 1280px)'
+    xsm: 'screen and (min-width: 320px)',
+    sm: 'screen and (min-width: 640px)',
+    md: 'screen and (min-width: 768px)',
+    lg: 'screen and (min-width: 1024px)',
+    xl: 'screen and (min-width: 1280px)'
   },
 }
