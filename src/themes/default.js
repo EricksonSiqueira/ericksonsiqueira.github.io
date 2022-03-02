54854f6ea65dcff2537097d737e19302e22cf67d
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-02 12:09:18 -0300

    update: fonte e cores

---
+++ src/themes/default.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  // Temp fonts
+  fonts: {
+    title: 'Roboto, sans-serif',
+    main: 'Roboto, sans-serif'
+  },
+  // Colors for layout
+  colors: {
+    background: '#0F1525',
+    textColor: '#e2e2e2',
+    darkPurple: '#110042',
+    lightPurple: '#340670',
+    greenText: '#15EC1D',
+    orangeText: '#CF9A10',
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    xsm: 'screen and (max-width: 320px)',
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+}
