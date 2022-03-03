165d4a1da0711abca61ec6c201b09bd5f3329b7e
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-02 21:32:09 -0300

    feat: responsividade do hero

--- src/components/hero/HeroStyle.js
+++ src/components/hero/HeroStyle.js
@@ -24,7 +24,67 @@ export const HeroStyled = styled.section`
     color: ${props => props.theme.colors.greenText};
   }
 
+    @media ${(props) => props.theme.breakpoints.sm} {
+    max-width: 600px;
+    margin: 0 auto;
+
+    img {
+      width: 80vh;
+    }
+  }
+
   @media ${(props) => props.theme.breakpoints.md} {
     flex-direction: row;
+    max-width: 800px;
+    margin: 0 auto;
+
+    img {
+      bottom: 0;
+      width: 80vh;
+    }
+    section {
+      margin-left: -70px;
+    }
+  }
+
+  @media ${(props) => props.theme.breakpoints.lg} {
+    max-width: 1000px;
+    margin: 0 auto;
+    img {
+      width: 80vh;
+    }
+    
+    section {
+      margin-left: 0px;
+      font-size: 22px;
+    }
+
+    button {
+      margin-top: 30px;
+      font-size: 18px;
+    }
+  }
+    @media ${(props) => props.theme.breakpoints.xl} {
+    flex-direction: row;
+    max-width: 1200px;
+    margin: 2% auto;
+
+    img {
+      width: 80vh;
+    }
+
+    section {
+      margin-left: -30px;
+      font-size: 28px;
+    }
+
+    h1 {
+      width: 500px;
+    }
+
+    button {
+      margin-top: 30px;
+      font-size: 18px;
+    }
   }
 `;
