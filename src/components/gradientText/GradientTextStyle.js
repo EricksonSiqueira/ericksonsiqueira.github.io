8ff788e08f4090d273b82ef44a81ff5bad0e6c10
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-14 01:48:35 -0300

    fix: remove font-size desnecessário

--- src/components/gradientText/GradientTextStyle.js
+++ src/components/gradientText/GradientTextStyle.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const GradientH1 = styled.h1`
-
-  font-size: ${props => `${props.fontSize}px`};
   background: #904AE8;
   background: -webkit-linear-gradient(to right, #904AE8 0%, #09CAF4 100%);
   background: -moz-linear-gradient(to right, #904AE8 0%, #09CAF4 100%);
