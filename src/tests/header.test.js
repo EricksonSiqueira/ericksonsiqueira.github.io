5018476d9eec700817d4618bc7f6ea38d6441dfb
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-22 22:18:05 -0300

    test: add test to header nav

---
+++ src/tests/header.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Header', () => {
+
+  describe('nav', () => {
+
+    it('has link with text "Home"', () => {
+      render(<App />)
+      const link = screen.getByRole('link', { name: /Home/ });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', '#home');
+    });
+    it('has link with text "Sobre"', () => {
+      render(<App />)
+      const link = screen.getByRole('link', { name: /Sobre/ });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', '#about');
+    });
+
+    it('has link with text "Tecnologias"', () => {
+      render(<App />)
+      const link = screen.getByRole('link', { name: /Tecnologias/ });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', '#technologies');
+    });
+
+    it('has link with text "Projetos"', () => {
+      render(<App />)
+      const link = screen.getByRole('link', { name: /Projetos/ });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', '#projects');
+    });
+
+    it('has link with text "Contato"', () => {
+      render(<App />)
+      const link = screen.getByRole('link', { name: /Contato/ });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', '#contact');
+    });
+  });
+
+  describe('social media section', () => {
+    it('has link with text "Contato"', () => {
+      render(<App />)
+      const link = screen.getByRole('link', { name: /Contato/ });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', '#contact');
+    });
+  });
+});
