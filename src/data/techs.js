e08d5ef2709a04143fdd88c7f19e93e15134f609
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-28 19:43:31 -0300

    feat: data dos icones de tecnologia

---
+++ src/data/techs.js
@@ -0,0 +1,77 @@
+import * as FaIcons from 'react-icons/fa';
+import * as SiIcons from 'react-icons/si';
+import * as GrIcons from 'react-icons/gr';
+
+
+const data = [
+  {
+    text: 'React',
+    Icon: FaIcons.FaReact,
+  },
+  {
+    text: 'JavaScript',
+    Icon: SiIcons.SiJavascript,
+  },
+  {
+    text: 'TypeScript',
+    Icon: SiIcons.SiTypescript,
+  },
+  {
+    text: 'Jest',
+    Icon: SiIcons.SiJest,
+  },
+  {
+    text: 'CSS3',
+    Icon: FaIcons.FaCss3Alt,
+  },
+  {
+    text: 'HTML5',
+    Icon: FaIcons.FaHtml5,
+  },
+  {
+    text: 'Git',
+    Icon: FaIcons.FaGitAlt,
+  },
+  {
+    text: 'GitHub',
+    Icon: FaIcons.FaGithub,
+  },
+  {
+    text: 'Styled components',
+    Icon: SiIcons.SiStyledcomponents
+  },
+  {
+    text: 'Tailwind',
+    Icon: SiIcons.SiTailwindcss,
+  },
+  {
+    text: 'Node.js',
+    Icon: FaIcons.FaNodeJs,
+  },
+  {
+    text: 'RTL',
+    Icon: SiIcons.SiTestinglibrary,
+  },
+  {
+    text: 'Express',
+    Icon: SiIcons.SiExpress,
+  },
+  {
+    text: 'Redux',
+    Icon: SiIcons.SiRedux,
+  },
+  {
+    text: 'MySql',
+    Icon: GrIcons.GrMysql,
+  },
+  {
+    text: 'Sequelize',
+    Icon: SiIcons.SiSequelize,
+  },
+  {
+    text: 'Docker',
+    Icon: GrIcons.GrDocker,
+  },
+];
+
+export default data;
