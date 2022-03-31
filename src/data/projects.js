06a7cb8b184b16cea146516660addfe8f0e93da2
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-31 15:32:52 -0300

    feat: add project cards data

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+import chefPediaImg from '../img/chef-pedia.png'
+
+const projects = [
+  {
+    image: chefPediaImg,
+    technologies: ['#ReactJs', '#React Hooks', '#ContextAPI', '#Tailwind CSS'],
+    githubLink:'https://github.com/EricksonSiqueira/chef-pedia',
+    deployLink: 'https://chefpedia.vercel.app/',
+  },
+  {
+    image: chefPediaImg,
+    technologies: ['#ReactJs', '#React Hooks', '#ContextAPI', '#Tailwind CSS'],
+    githubLink: 'https://github.com/EricksonSiqueira/chef-pedia',
+    deployLink: 'https://ericksonsiqueira.github.io/',
+  },
+  {
+    image: chefPediaImg,
+    technologies: ['#ReactJs', '#React Hooks', '#ContextAPI', '#Tailwind CSS'],
+    githubLink: 'https://github.com/EricksonSiqueira/chef-pedia',
+    deployLink: 'https://tryunfo111.herokuapp.com/',
+  },
+];
+
+export default projects;
