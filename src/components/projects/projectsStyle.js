b40ba1696842027c4475de52e30f368b752bbdc8
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-31 15:29:59 -0300

    create: projects styles

---
+++ src/components/projects/projectsStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledProjects = styled.section`
+
+  section.cards {
+    width: 100%;
+    margin-top: 30px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    row-gap: 50px;
+  }
+
+  .tech-heading {
+    font-size: 50px;
+    width: 190px;
+  }
+`;
