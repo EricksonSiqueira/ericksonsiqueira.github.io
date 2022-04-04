eb33db9d3874dd4c29cd1674723dce36d0c823c1
Erickson <ericksonsiqueirasilva@gmail.com>
2022-04-04 00:20:20 -0300

    feat: contact component

---
+++ src/components/contact/index.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { StyledContact } from './ContactStyle';
+import GradientText from '../gradientText/GradientText';
+import SectionDivider from '../sectionDivider/SectionDivider';
+import GradientBtn from '../gradientBtn/GradientBtn';
+import { send } from 'emailjs-com';
+
+const Contact = () => {
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [message, setMessage] = useState('');
+
+  const TO_SEND_INITIAL_STATE = {
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+  const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+  const USER_ID = process.env.REACT_APP_USER_ID;
+
+  const [toSend, setToSend] = useState(TO_SEND_INITIAL_STATE);
+
+  const sendForm = (event) => {
+    event.preventDefault();
+    send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      toSend,
+      USER_ID,
+    )
+      .then((response) => {
+        window.alert('Enviado!');
+        console.log(response.status, response.text);
+      })
+      .catch((err) => {
+        window.alert('FAILED...');
+        console.log(err);
+      });
+
+    setToSend(TO_SEND_INITIAL_STATE);
+  }
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <StyledContact id="contact" className="section-spacing">
+      <SectionDivider />
+      <GradientText text="Contato" classes="contact-heading"/>
+      <form>
+        <label htmlFor="name-input">
+          <input 
+            type="text"
+            name="name"
+            id="name-input"
+            value={ toSend.name }
+            onChange={ handleChange }
+            placeholder="nome"
+          />
+        </label>
+        <label htmlFor="email-input">
+          <input 
+            type="text"
+            name="email"
+            id="email-input"
+            value={ toSend.email }
+            onChange={ handleChange }
+            placeholder="email"
+          />
+        </label>
+        <label htmlFor="subject-input">
+          <textarea
+            name="message"
+            id="subject-input"
+            cols="30" rows="10"
+            value={ toSend.message }
+            onChange={ handleChange }
+            placeholder="mensagem"
+          />
+        </label>
+        <GradientBtn text={'Enviar'} onClickFunc={ sendForm }/>
+      </form>
+    </StyledContact>
+  );
+};
+
+export default Contact;
