52b4387988f3e9f9ee360e19ac4e08b5d29aaefd
Erickson <ericksonsiqueirasilva@gmail.com>
2022-04-04 23:07:00 -0300

    feat: change scroll style

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -21,7 +21,24 @@ const GlobalStyles = createGlobalStyle`
   }
   body {
     background-color: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.textColor}
+    color: ${props => props.theme.colors.textColor};
+
+    &::-webkit-scrollbar {
+      width: 0.8rem;
+    }
+
+    &::-webkit-scrollbar-track {
+      background-color: ${(props) => props.theme.colors.darkPurple};
+      box-shadow: inset 0.05 0 0 #838383;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: green;
+      border-radius: 0.5rem;
+      background: rgb(136,70,219);
+      background: linear-gradient(180deg, rgba(136,70,219,1) 0%, rgba(13,198,243,1) 100%);
+      box-shadow: -3px 4px 15px 5px #000000;
+    }
   }
 
   h1,h2,h3,h4,h5,h6,button {
