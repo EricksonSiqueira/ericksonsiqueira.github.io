a25cb230c72760158a988b662419908c0e3933a2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-04 17:21:38 -0300

    feat: add new project to page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,29 @@
-import chefPediaImg from '../img/chef-pedia.png'
-import portfolioImg from '../img/portfolio.png'
-import tryunfoImg from '../img/tryunfo.png'
+import chefPediaImg from '../img/chef-pedia.png';
+import portfolioImg from '../img/portfolio.png';
+import loginScreen from '../img/login-screen.png';
 
 const projects = [
   {
+    image: loginScreen,
+    technologies: ['TypeScript', 'Next.js', 'Tailwindcss', 'Jest', 'Yup'],
+    githubLink: 'https://github.com/EricksonSiqueira/mp-tela-login-tailwind',
+    deployLink: 'https://tela-login-tailwind-three.vercel.app/',
+    description: `Tela de login em Next.js, com estilos em Tailwindcss e validações com Yup. Design feito no figma.`,
+  },
+  {
     image: chefPediaImg,
-    technologies: ['#ReactJs', '#React Hooks', '#ContextAPI', '#Tailwind CSS'],
-    githubLink:'https://github.com/EricksonSiqueira/chef-pedia',
+    technologies: ['ReactJs', 'React Hooks', 'ContextAPI', 'Tailwind CSS'],
+    githubLink: 'https://github.com/EricksonSiqueira/chef-pedia',
     deployLink: 'https://chefpedia.vercel.app/',
     description: `Um site de receitas onde você pode procurar comidas e bebidas por diversos filtros, iniciar uma receita, favoritar e muito mais!`,
   },
   {
     image: portfolioImg,
-    technologies: ['#ReactJs', '#React Hooks', 'Styled components', '#RTL'],
-    githubLink: 'https://github.com/EricksonSiqueira/ericksonsiqueira.github.io',
+    technologies: ['ReactJs', 'React Hooks', 'Styled components', 'RTL'],
+    githubLink:
+      'https://github.com/EricksonSiqueira/ericksonsiqueira.github.io',
     deployLink: 'https://ericksonsiqueira.github.io/',
     description: `Meu portfolio, onde falo um pouco sobre eu mesmo e as tecnologias que conheço (esse site que você está utilizando)`,
-  },
-  {
-    image: tryunfoImg,
-    technologies: ['#ReactJs', '#CSS3', '#JavaScript', '#HTML5'],
-    githubLink: 'https://github.com/EricksonSiqueira/tryunfo',
-    deployLink: 'https://tryunfo111.herokuapp.com/',
-    description: `Site onde você pode criar suas próprias cartas do jogo trunfo! Use sua imaginação para criar quantas quiser!`,
   },
 ];
 
